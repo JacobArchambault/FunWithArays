a1266a78d024a29523889c2cfda212725f22d2ac
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-15 10:31:59 -0400

    Write SimpleArrays() method

--- FunWithArays/Program.cs
+++ FunWithArays/Program.cs
@@ -10,6 +10,25 @@ namespace FunWithArays
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Arrays *****");
+            SimpleArrays();
+            Console.ReadLine();
+        }
+
+        static void SimpleArrays()
+        {
+            Console.WriteLine("=> Simple Array Creation.");
+            // Create an array of ints containing 3 elements indexed 0, 1, 2
+            int[] myInts = new int[3];
+            myInts[0] = 100;
+            myInts[1] = 200;
+            myInts[2] = 300;
+
+            // Now print each value.
+            foreach (int i in myInts)
+                Console.WriteLine(i);
+
+            Console.WriteLine();
         }
     }
 }
